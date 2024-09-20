7db3e9f6eb30522534984d953c36704b3242662c
rachanatkr <145425935+rachanatkr@users.noreply.github.com>
2024-09-20 14:56:57 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+class Employee 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("abc");
+		System.out.println(77777);
+		System.out.println(8888888);
+		System.out.println(11);
+		System.out.println('m');
+		System.out.println(true);
+	}
+}
